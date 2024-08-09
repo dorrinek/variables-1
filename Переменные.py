10b6a4af6977569f56f8e7436f3a88d10dfb8494
Alex <alex14062006ggglol@gmail.com>
2024-08-09 18:47:10 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+first_name = 'Курс:Python, '
+total_task = ':12,'
+hours_spent = ':1.5,'
+time = '0.125'
+print(first_name, 'всего задач', total_task, 'затрачено часов', hours_spent, 'среднее время выполнения', time, 'часа.')
