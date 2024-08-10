19f28962df32ecc30aa6859a069a7564a3f09f2e
Alex <alex14062006ggglol@gmail.com>
2024-08-10 17:26:52 +0300

    Delete Переменные.py

--- Переменные.py
+++
@@ -1,5 +0,0 @@
-first_name = 'Курс:Python, '
-total_task = ':12,'
-hours_spent = ':1.5,'
-time = '0.125'
-print(first_name, 'всего задач', total_task, 'затрачено часов', hours_spent, 'среднее время выполнения', time, 'часа.')
